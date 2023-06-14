edfc2409ade5143765465b13417317dd40c15fb6
Domingos Alves <73261252+domrms@users.noreply.github.com>
2023-06-14 18:50:43 -0300

    Atividade: Um arquivo possui permissões de acesso.

---
+++ Trabalhos/T08/Arquivo.cs
@@ -0,0 +1,12 @@
+﻿namespace Trabalhos.T08
+{
+    public class Arquivo
+    {
+        public List<Permissao> Permissoes { get; set; }
+
+        public Arquivo()
+        {
+            Permissoes = new List<Permissao>();
+        }
+    }
+}
---
+++ Trabalhos/T08/Permissao.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Trabalhos.T08
+{
+    public class Permissao
+    {
+        public Grupo GrupoAssociado { get; set; }
+        public Arquivo ArquivoAssociado { get; set; }
+    }
+}
