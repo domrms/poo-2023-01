aca56a5b8385172f5ac9f26c6868c7a9d93d28e3
Domingos Alves <73261252+domrms@users.noreply.github.com>
2023-07-02 21:23:06 -0300

    T09.09

--- 01. Trabalhos/T09.10/Viagem.cs
+++ 01. Trabalhos/T09.10/Viagem.cs
@@ -9,38 +9,4 @@
             Cidades = cidades;
         }
     }
-
-    public class CaixeiroViajante
-    {
-        public List<Viagem> Viagens { get; private set; }
-
-        public CaixeiroViajante()
-        {
-            Viagens = new List<Viagem>();
-        }
-
-        public void AdicionarViagem(Viagem viagem)
-        {
-            Viagens.Add(viagem);
-        }
-
-        public void ListarViagens()
-        {
-            Console.WriteLine("Lista de viagens do caixeiro-viajante:");
-            foreach (Viagem viagem in Viagens)
-            {
-                Console.WriteLine("Viagem:");
-                foreach (Cidade cidade in viagem.Cidades)
-                {
-                    Console.WriteLine($"Cidade: {cidade.Nome}");
-                    Console.WriteLine("Pedidos de Armazéns:");
-                    foreach (Armazem pedido in cidade.Pedidos)
-                    {
-                        Console.WriteLine(pedido.Nome);
-                    }
-                    Console.WriteLine();
-                }
-            }
-        }
-    }
 }
