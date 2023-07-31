577cbe3beb079bb666a3baa2ac751c7a711375fd
Domingos Alves <domingosallves@outlook.com>
2023-07-30 22:49:12 -0300

    Refatoração do readme do T08.

---
+++ t08/t08.11/Arquivo.cs
@@ -0,0 +1,13 @@
+﻿namespace T08._11
+{
+    public class Arquivo
+    {
+        public List<Permissao> Permissoes { get; set; }
+
+        public Arquivo()
+        {
+            Permissoes = new List<Permissao>();
+        }
+
+    }
+}
---
+++ t08/t08.13/Pessoa.cs
@@ -0,0 +1,13 @@
+﻿namespace T08._13
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public List<Empresa> Empregadores { get; } = new List<Empresa>();
+
+        public void AdicionarEmpregador(Empresa empregador)
+        {
+            Empregadores.Add(empregador);
+        }
+    }
+}
