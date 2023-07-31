29ebe1a6f4c4e29f40679c301705c2b1c42c1985
Domingos Alves <domingosallves@outlook.com>
2023-07-30 22:54:57 -0300

    Refatoração do T10.

---
+++ t10/Homem.cs
@@ -0,0 +1,9 @@
+﻿namespace T10
+{
+    public class Homem
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+    }
+}
---
+++ t10/MainMethod.cs
@@ -0,0 +1,14 @@
+﻿namespace T10
+{
+    public class MainMethod
+    {
+        static void Main()
+        {
+            Homem homem = new Homem { Nome = "João", Idade = 30 };
+            Bicicleta bicicleta = new Bicicleta { Marca = "Caloi", Modelo = "XRT" };
+
+            HomemNaBicicleta homemNaBicicleta = new HomemNaBicicleta(homem, bicicleta);
+            homemNaBicicleta.Pedalar();
+        }
+    }
+}
